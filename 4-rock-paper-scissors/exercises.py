b356370409871eaa64aa1f0771f0a393528b7801
Michael Nguyen <n93michael@gmail.com>
2022-12-31 11:07:08 -0500

    lists and list methods

--- 4-rock-paper-scissors/exercises.py
+++ 4-rock-paper-scissors/exercises.py
@@ -2,6 +2,7 @@ import random
 
 # HEAD OR TAILS GENERATOR #######
 
+# Generates a float between 0-0.99
 random_value = round(random.random())
 
 if random_value == 1:
--- 4-rock-paper-scissors/main.py
+++ 4-rock-paper-scissors/main.py
@@ -20,3 +20,25 @@ random_float = random.random()  # Returns a float from 0-0.99
 
 # Using random() to generate a number between 0-5, no including 5
 print(round(random_float * 5, 1))
+
+
+# Lists aka Arrays
+
+# Lists are ordered, via index, and can store any data type, just like JS
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut"]
+
+first_state = states_of_america[0]  # "Delaware"
+last_state = states_of_america[-1]  # "Connecticut
+
+states_of_america[1] = "Pencilvania"
+
+# append() add an item to the end of the list
+states_of_america.append("Michael-Land")
+
+# extend() adds a list to the end of the list
+states_of_america.extend(["Irene", "Henry"])
+
+print(states_of_america)
+
+
