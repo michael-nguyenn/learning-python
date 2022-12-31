6e7480373470b0c542e1a5313c5c0599906d4215
Michael Nguyen <n93michael@gmail.com>
2022-12-31 12:01:03 -0500

    day 4 exercise 3: treasure map marker

--- 4-rock-paper-scissors/exercises.py
+++ 4-rock-paper-scissors/exercises.py
@@ -25,3 +25,25 @@ index = random.randint(0, len(names) - 1)
 
 print(f"{names[index]} is going to buy the meal today!")
 
+
+# TREASURE MAP
+# Marking a spot with an x. Input is 2 digits coordinates that will correspond to a grid of 9, created
+# with nested lists
+
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+
+map = [row1, row2, row3]
+
+# position = input("Where do you want to put the treasure? ")
+position = 23
+
+# Converting user input to usable format
+index_coordinates = list(str(position))
+row_index = int(index_coordinates[1]) - 1
+column_index = int(index_coordinates[0]) - 1
+
+
+map[row_index][column_index] = "X"
+print(f"{row1}\n{row2}\n{row3}")
