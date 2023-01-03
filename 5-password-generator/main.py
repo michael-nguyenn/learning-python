687733c62a6bb3bbbe82ac120931c632a7ca03ad
Michael Nguyen <n93michael@gmail.com>
2023-01-02 21:11:36 -0500

    introduction to for loop

---
+++ 5-password-generator/main.py
@@ -0,0 +1,12 @@
+# Loops
+
+# For Loop - Loop through anything that is iterable. e.g. a range, a list, a dictionary, or tuple
+
+fruits = ['Apple', 'Peach', 'Pear']
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+
+print(fruits)
+
