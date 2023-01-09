491cbb15a730e523821a23af8280c9eb966dcee0
Michael Nguyen <n93michael@gmail.com>
2023-01-09 13:21:53 -0500

    day 8 exercise 2: prime number checker

--- 8-caesar-cipher/exercise.py
+++ 8-caesar-cipher/exercise.py
@@ -9,3 +9,38 @@ def paint_calc(height, width, cover):
 
 
 paint_calc(2, 4, 5)
+
+
+# Coding Exercise 2 - Prime Number Checker
+
+# Solution # 1
+def prime_checker(number):
+    multiple_of_number = 0
+    for i in range(1, number + 1):
+        if number % i == 0:
+            multiple_of_number += 1
+
+    if multiple_of_number > 2:
+        print("It's not a prime number.")
+    else:
+        print("It's a prime number.")
+
+
+prime_checker(11)
+
+
+# Solution # 2
+def prime_checker_2(number):
+    is_prime = True
+
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+
+    if is_prime:
+        print("It's a prime number")
+    else:
+        print("It's not a prime number")
+
+
+prime_checker_2(11)
