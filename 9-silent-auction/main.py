4286a5d6fc5610c55cc2ab791a8753c75149db65
Michael Nguyen <n93michael@gmail.com>
2023-01-10 09:56:28 -0500

    nesting lists and dictionaries with examples

--- 9-silent-auction/main.py
+++ 9-silent-auction/main.py
@@ -1,4 +1,4 @@
-# Dictionaries ! aka objects
+# Dictionaries ! aka objects ###########
 
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.",
@@ -29,3 +29,45 @@ for key in programming_dictionary:
     print(key)
     # Using the key to loop through it's values
     print(programming_dictionary[key])
+
+# Nesting Lists and Dictionaries ########
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# Nesting a List in a Dictionary
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+
+# Nesting Dictionary in a Dictionary
+
+travel_log_two = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12,
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 8,
+    },
+}
+
+# Nesting a dictionary in a List
+
+travel_log_three = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 8
+    },
+]
