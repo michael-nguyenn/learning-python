407b9812aecf073c8b2248ffee5f3cf8c68a734f
Michael Nguyen <n93michael@gmail.com>
2023-01-11 11:41:25 -0500

    intro to docstrings

--- 10-calculator/main.py
+++ 10-calculator/main.py
@@ -12,11 +12,14 @@ output = my_function()  # 6
 # Changing the word to title case
 # Using the str.title()
 def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of the name."""
     if f_name == "" or l_name == "":
         return "Please input a valid input"
 
     return f_name.title() + " " + l_name.title()
 
 
+# Docstring
+# Note when we hover over this line, you can see the docstring we created within the function def
 my_name = format_name("Michael", "NGUYEN")  # Michael Nguyen
 # print(my_name)
