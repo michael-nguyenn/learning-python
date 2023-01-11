ab6746e1ecaad9c21990dd5785d7cd7d91de5ddc
Michael Nguyen <n93michael@gmail.com>
2023-01-11 11:16:15 -0500

    using multiple return values within a function

--- 10-calculator/main.py
+++ 10-calculator/main.py
@@ -12,7 +12,11 @@ output = my_function()  # 6
 # Changing the word to title case
 # Using the str.title()
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "Please input a valid input"
+
     return f_name.title() + " " + l_name.title()
 
 
-my_name = format_name("michael", "NGUYEN")  # Michael Nguyen
+my_name = format_name("Michael", "NGUYEN")  # Michael Nguyen
+# print(my_name)
