1dd51f7d642dd78db63e48032e49f0d215b1c55c
Michael Nguyen <n93michael@gmail.com>
2023-01-12 16:33:32 -0500

    day 11 black jack - assigning user/computer random cards, checking if blackjack is achieved, created function to switch ace values

--- 11-black-jack/black-jack.py
+++ 11-black-jack/black-jack.py
@@ -23,5 +23,37 @@ def clear(seconds=0):
 # Deck of Cards Used
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+
+# Ace Checker
+def ace_checker(hand):
+    for i in range(len(hand)):
+        print(i)
+        if hand[i] == 11 and sum(hand) > 21:
+            hand[i] = 1
+
+
 # Displaying Logo
 # print(logo)
+
+
+# User and Computer Hands
+user_hand = []
+computer_hand = []
+
+# Randomly Assigning 2 cards each
+for _ in range(2):
+    user_hand.append(random.choice(cards))
+    computer_hand.append(random.choice(cards))
+
+print(f"User Hand:{user_hand}\nComputer Hand:{computer_hand}")
+
+# Order matters, because if both players have blackjack, computer will win
+if sum(computer_hand) == 21:
+    print(f"Computer got black jack {computer_hand}! You lose...")
+    time.sleep(1)
+    # print("Would you like to play again?")
+
+if sum(user_hand) == 21:
+    print(f"BlackJack {user_hand}! You win~")
+    time.sleep(1)
+    # print("Would you like to play again?")
