d805aaca09cf0edb13d9988d02581134945e7d3e
Michael Nguyen <n93michael@gmail.com>
2023-01-18 10:10:24 -0500

    day 14 higher lower game - initial folder set up, and pseudocode done

---
+++ 13-higher-lower-game/high-lower.py
@@ -0,0 +1,20 @@
+# Higher Lower Game...
+# Game is designed to give the user a choice between two persons from a list, and choose who has more followers.
+# If chosen correctly, you continue on until you lose, then we give the score
+
+# From the dictionary, randomly select a person from the list, and assign it to A, and B
+#   ?Create a function to random retrieve a person
+
+# Then ask user to choose who has more followers
+#   If they guess correctly, the correct answer becomes option A
+#   Randomly select a person from the list and assign to person B
+#   Tally the person's correct guesses
+
+#   If they guess wrong, then the game ends, we let the user know how many correct guesses they had
+
+
+import art
+import random
+
+print(art.logo)
+print(art.vs)
