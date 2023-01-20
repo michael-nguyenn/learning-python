da435dab1ce96a4531443bc019f6a3b901efc5bb
Michael Nguyen <n93michael@gmail.com>
2023-01-20 13:11:03 -0500

    day 15 coffee machine - created coin process function, created resources function, implemented coin processing, refund, and adding payment to machine

--- 15-coffee-machine/coffee-machine.py
+++ 15-coffee-machine/coffee-machine.py
@@ -1,8 +1,6 @@
 import sys
 import data
 
-money = 0.0
-
 
 def resource_check(drink):
     """Checking if there are enough resources in the machine"""
@@ -27,12 +25,31 @@ def resource_check(drink):
     return enough_ingredients
 
 
+def coin_process():
+    """Asks users for coins, and processes total amount"""
+    print("Please insert your coins")
+    amount = int(input("How many quarters?: ")) * 0.25
+    amount += int(input("How many dimes?: ")) * 0.10
+    amount += int(input("How many nickels?: ")) * 0.05
+    amount += int(input("How many pennies?: ")) * 0.01
+
+    return amount
+
+
+def resources_avail(money):
+    print(f"Water: {data.resources['water']}")
+    print(f"Milk: {data.resources['milk']}")
+    print(f"Coffee: {data.resources['coffee']}")
+    print(f"Money: ${money}")
+
+
 def coffee_machine():
+    money = 0.0
     functional = True
 
     while functional:
         # 1 - Prompt user by asking, "What would you like? (expresso/latte/cappuccino): "
-        #  TODO - THIS PROMPT SHOULD SHOW EVERYTIME AN ACTION HAS BEEN COMPLETED
+        #  Prompt will show everytime we complete an action
         user_input = input("What would you like? (espresso/latte/cappuccino): ")
 
         # TESTING CODE
@@ -44,15 +61,29 @@ def coffee_machine():
 
         # 3 - Print Report - "report"
         if user_input == 'report':
-            print(f"Water: {data.resources['water']}")
-            print(f"Milk: {data.resources['milk']}")
-            print(f"Coffee: {data.resources['coffee']}")
-            print(f"Money: ${money}")
+            resources_avail(money)
 
         # 4 - Checking if resources are sufficient
         if user_input == 'espresso' or user_input == 'latte' or user_input == 'cappuccino':
             if not resource_check(user_input):
+                # TODO - If resource is not sufficient, ask user to make another input.
+                #  If all choices have been exhausted, then we end the program
                 functional = False
+
+            refund = True
+            item_price = data.MENU[user_input]["cost"]
+
+            while refund:
+                # 5 - Processing coins
+                user_cash = coin_process()
+
+                # 6 - Check if transaction is successful
+                if item_price > user_cash:
+                    print("Sorry that's not enough money. Money refunded")
+                else:
+                    money += item_price
+                    resources_avail(money)
+                    refund = False
 
     # TESTING CODE
     print("TESTING CODE - Over")
