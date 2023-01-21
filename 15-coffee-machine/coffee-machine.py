1510eb4d29ce948df70bc1f14c46b730f307092c
Michael Nguyen <n93michael@gmail.com>
2023-01-21 12:18:42 -0500

    day 15 coffee machine - completed functionality

--- 15-coffee-machine/coffee-machine.py
+++ 15-coffee-machine/coffee-machine.py
@@ -1,11 +1,13 @@
-import sys
+# import sys
 import data
+import time
+from art import logo
 
 
 def resource_check(drink):
     """Checking if there are enough resources in the machine"""
     # TESTING CODE
-    print("TESTING CODE - Resource Check is running...")
+    # print("TESTING CODE - Resource Check is running...")
 
     # Accessing the ingredients keys from selected drink
     ingredients = data.MENU[drink]['ingredients']
@@ -43,17 +45,38 @@ def resources_avail(money):
     print(f"Money: ${money}")
 
 
+def brew(drink):
+    """Loop through current drink, and subtract from resource"""
+
+    ingredients = data.MENU[drink]['ingredients']
+
+    for ingredient in ingredients:
+        data.resources[ingredient] -= ingredients[ingredient]
+
+
+def list_prices(menu):
+    """Loop through each object, and print out cost"""
+    for item in menu:
+        print(f"{item.capitalize()} costs ${menu[item]['cost']}")
+
+
 def coffee_machine():
+    print(logo)
+    print("Would you like a hot drink?")
+    time.sleep(1)
+    list_prices(data.MENU)
+
     money = 0.0
     functional = True
 
     while functional:
+
         # 1 - Prompt user by asking, "What would you like? (expresso/latte/cappuccino): "
         #  Prompt will show everytime we complete an action
         user_input = input("What would you like? (espresso/latte/cappuccino): ")
 
         # TESTING CODE
-        print(f"TESTING CODE - {user_input}")
+        # print(f"TESTING CODE - {user_input}")
 
         # 2 - Turn off Coffee Machine - "off"
         if user_input == 'exit':
@@ -66,27 +89,36 @@ def coffee_machine():
         # 4 - Checking if resources are sufficient
         if user_input == 'espresso' or user_input == 'latte' or user_input == 'cappuccino':
             if not resource_check(user_input):
-                # TODO - If resource is not sufficient, ask user to make another input.
-                #  If all choices have been exhausted, then we end the program
                 functional = False
+            else:
+                refund = True
+                item_price = data.MENU[user_input]["cost"]
 
-            refund = True
-            item_price = data.MENU[user_input]["cost"]
+                while refund:
+                    # 5 - Processing coins
+                    user_cash = coin_process()
 
-            while refund:
-                # 5 - Processing coins
-                user_cash = coin_process()
+                    # TESTING CODE
+                    # print(f"TESTING CODE - {user_cash}")
 
-                # 6 - Check if transaction is successful
-                if item_price > user_cash:
-                    print("Sorry that's not enough money. Money refunded")
-                else:
-                    money += item_price
-                    resources_avail(money)
-                    refund = False
+                    # 6 - Check if transaction is successful
+                    if item_price > user_cash:
+                        print("Sorry that's not enough money. Money refunded")
+                    else:
+                        print(f"Here is ${round((user_cash - item_price), 2)} in change")
+                        refund = False
 
-    # TESTING CODE
-    print("TESTING CODE - Over")
+                # 7 - MAKE COFFEE
+                print(f"Report before purchasing {user_input}")
+                time.sleep(1)
+                resources_avail(money)
+                print(f"--- BREWING UP YOUR {user_input.upper()} ---")
+                time.sleep(1)
+                brew(user_input)
+                money += item_price
+                print(f"Report after purchasing {user_input}:")
+                time.sleep(1)
+                resources_avail(money)
 
 
 coffee_machine()
