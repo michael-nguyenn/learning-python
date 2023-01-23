c1313349116dea10ad43e3c94868280795a2c5fe
Michael Nguyen <n93michael@gmail.com>
2023-01-23 15:29:03 -0500

    day 17 - creating methods in classes

--- 17-quiz-oop/main.py
+++ 17-quiz-oop/main.py
@@ -15,10 +15,24 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0  # Providing a default value
+        self.following = 0
+
+    # Creating a method
+    def follow(self, user):
+        user.followers += 1
+        # The self keyword, similar to this, refers to the object that will be created from this class
+        self.following += 1
 
 
-user_1 = User("001", "michael")
-user_2 = User("002", "irene")
+user_1 = User("001", "michael")  # new user being created
+user_2 = User("002", "irene")  # new user being created
 
 # Creating attributes - not the best practice
 user_1.test = True
+
+# Calling the method
+user_1.follow(user_2)
+
+print(user_1.following)  # 1
+print(user_1.followers)  # 0
+print(user_2.followers)  # 1
