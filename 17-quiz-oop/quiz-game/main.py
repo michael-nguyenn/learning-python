5e14649cea69f0a375abf521dde11ba1acde8c69
Michael Nguyen <n93michael@gmail.com>
2023-01-23 15:48:54 -0500

    day 17 quiz oop - creating a list of Question objects from data set

--- 17-quiz-oop/quiz-game/main.py
+++ 17-quiz-oop/quiz-game/main.py
@@ -1,1 +1,13 @@
+from question_model import Question
+from data import question_data
 
+# Need to iterate over our list of dictionaries, and create a Question
+#   object for each entry in question_data
+# Append each Question object to the question_bank
+
+question_bank = []
+
+for i in range(len(question_data)):
+    question = question_data[i]['text']
+    answer = question_data[i]['answer']
+    question_bank.append(Question(question, answer))
--- 17-quiz-oop/quiz-game/question_model.py
+++ 17-quiz-oop/quiz-game/question_model.py
@@ -2,8 +2,3 @@ class Question:
     def __init__(self, question, answer):
         self.question = question
         self.answer = answer
-
-
-new_q = Question("2+3=5", "True")
-
-print(new_q.question)
