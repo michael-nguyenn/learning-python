68d999283c1d02b2060c1f6411ba8bdced4013c8
Michael Nguyen <n93michael@gmail.com>
2023-01-25 10:21:09 -0500

    day 18 challenge #3 - drawing triangle up to decagon

--- 18-hirst-painting/main.py
+++ 18-hirst-painting/main.py
@@ -1,6 +1,9 @@
 # Understanding the Turtle Graphics library and reading documentation
 
 from turtle import Turtle, Screen
+from random import choice
+
+colors = ['SkyBlue', 'Aquamarine', 'indigo', 'DeepPink', 'Tomato', 'DarkOrchid', 'LightSeaGreen', 'SlateGray']
 
 
 # Challenge #1 - Draw a square
@@ -21,11 +24,25 @@ def draw_dashed_line(turtle, lines):
         turtle.pendown()
 
 
+def draw_shapes(turtle):
+    # Want range to increment 1 each time
+    # while modulus of 360 % n is 0, then run the function
+
+    n = 3
+
+    while n <= 10:
+        for _ in range(n):
+            turtle.fd(100)
+            turtle.right(360 / n)
+        n += 1
+        turtle.color(choice(colors))
+
+
 tim = Turtle()
 tim.shape('turtle')
 tim.color('blueviolet')
 
-draw_dashed_line(tim, 10)
+draw_shapes(tim)
 
 screen = Screen()
 screen.exitonclick()
