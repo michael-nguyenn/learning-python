bfd8a340b1c072adaab9f6d7781eaeecd4f87230
Michael Nguyen <n93michael@gmail.com>
2023-01-25 09:56:25 -0500

    day 18 challenge #2 - drawing a dashed line

--- 18-hirst-painting/main.py
+++ 18-hirst-painting/main.py
@@ -3,17 +3,29 @@
 from turtle import Turtle, Screen
 
 
-def move_in_square(turtle, distance):
+# Challenge #1 - Draw a square
+def draw_square(turtle, distance):
+    """Will draw a square with the specified distance"""
     for _ in range(4):
         turtle.fd(distance)
         turtle.right(90)
+
+
+# Challenge # 2 - Draw a dashed line
+def draw_dashed_line(turtle, lines):
+    """The amount of dashed lines you want to draw"""
+    for _ in range(lines):
+        turtle.fd(10)
+        turtle.penup()
+        turtle.fd(10)
+        turtle.pendown()
 
 
 tim = Turtle()
 tim.shape('turtle')
 tim.color('blueviolet')
 
-move_in_square(tim, 100)
+draw_dashed_line(tim, 10)
 
 screen = Screen()
 screen.exitonclick()
