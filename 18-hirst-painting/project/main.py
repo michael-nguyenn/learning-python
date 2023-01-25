6cf3851d07f3b489ad47ba29c1bc914c2aed2d72
Michael Nguyen <n93michael@gmail.com>
2023-01-25 12:41:15 -0500

    day 18 hirst project - completed

--- 18-hirst-painting/project/main.py
+++ 18-hirst-painting/project/main.py
@@ -1,9 +1,9 @@
 import colorgram
+import random
+import turtle as t
 
 c = colorgram.extract('dots.jpg', 11)
 
-
-# print(colors[1].rgb[0])
 
 def rgb_extractor(colors):
     palette = []
@@ -16,4 +16,32 @@ def rgb_extractor(colors):
     return palette
 
 
-color_palette = rgb_extractor(c)
+color_list = rgb_extractor(c)
+
+
+def print_dots(turtle):
+    for _ in range(10):
+        turtle.dot(20, random.choice(color_list))
+        turtle.penup()
+        turtle.fd(50)
+        turtle.pendown()
+
+
+timmy = t.Turtle()
+t.colormode(255)  # Changing color mode so we can change to random colours
+timmy.penup()
+timmy.setx(-100)
+timmy.sety(-100)
+timmy.pendown()
+timmy.speed('fast')
+y = -100
+
+for _ in range(10):
+    print_dots(timmy)
+    timmy.penup()
+    y += 50
+    timmy.setpos(-100, y)
+    timmy.pendown()
+
+screen = t.Screen()
+screen.exitonclick()
