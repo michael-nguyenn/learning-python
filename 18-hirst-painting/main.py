697af609172e9d0dbc6ae36f1595bea720867200
Michael Nguyen <n93michael@gmail.com>
2023-01-24 22:39:25 -0500

    day 18 using turtle library

---
+++ 18-hirst-painting/main.py
@@ -0,0 +1,12 @@
+# Understanding the Turtle Graphics library and reading documentation
+
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+turtle.shape('turtle')
+turtle.color('blueviolet')
+turtle.fd(200)
+turtle.right(90)
+
+screen = Screen()
+screen.exitonclick()
