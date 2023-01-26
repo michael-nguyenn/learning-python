85082f10612fc2de41016aa27e6202e943d334a4
Michael Nguyen <n93michael@gmail.com>
2023-01-26 11:23:35 -0500

    day 19 exercise 1 - creating etch-a-sketch

--- 19-turtle-coord-system/etch-a-sketch.py
+++ 19-turtle-coord-system/etch-a-sketch.py
@@ -13,9 +13,29 @@ def move_forwards():
     tim.forward(10)
 
 
+def move_backwards():
+    tim.backward(10)
+
+
+def move_counter():
+    tim.left(10)
+
+
+def move_clockwise():
+    tim.right(10)
+
+
+def clear():
+    tim.reset()
+
+
 screen.listen()
 # When passing in functions as arguments, we don't include the ()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_counter)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
 
 # A higher order function can work with other functions. In our case above, move_forward is a higher
