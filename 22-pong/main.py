99ab8bfda3cb5927e762eee6dbdd694a571e0112
Michael Nguyen <n93michael@gmail.com>
2023-01-29 10:51:51 -0500

    day 22 pong - creating paddle class

--- 22-pong/main.py
+++ 22-pong/main.py
@@ -12,11 +12,24 @@
 #   Once the score gets incremented, we then create a new block
 
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title("Pong")
 screen.colormode(255)
+screen.tracer(0)
+
+player = Paddle(350)
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+screen.onkey(player.go_down, "Down")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
---
+++ 22-pong/paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, x):
+        super().__init__()
+        self.color('white')
+        self.shape('square')
+        self.turtlesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.x = x
+        self.goto(x=x, y=0)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
