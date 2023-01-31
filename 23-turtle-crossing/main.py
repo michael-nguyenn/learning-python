c89fc328f937d2fb2f50e098476ef288c392050e
Michael Nguyen <n93michael@gmail.com>
2023-01-31 11:26:05 -0500

    day 23 turtle crossing - create car behaviour

--- 23-turtle-crossing/main.py
+++ 23-turtle-crossing/main.py
@@ -2,12 +2,16 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
+import random
 from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
+
+# This initializes the car manager class
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.move, "Up")
@@ -17,5 +21,9 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    # Since our while loop runs at a 0.1s delay, it will generate a new car and call the move function
+    car_manager.create_car()
+    car_manager.move_cars()
+    
     # Detecting successful crossing
     player.has_crossed()
