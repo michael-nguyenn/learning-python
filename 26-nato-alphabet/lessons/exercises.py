12c0c58350fbe0b33e04ff19f6f1d0263d6ad543
Michael Nguyen <n93michael@gmail.com>
2023-02-05 19:30:57 -0500

    day 26 dictionary comprehension exercises

--- 26-nato-alphabet/lessons/exercises.py
+++ 26-nato-alphabet/lessons/exercises.py
@@ -23,3 +23,27 @@ print(file_2)
 duplicate_values = [number for number in file_1 if number == number in file_2]
 
 print(duplicate_values)
+
+# Using Dictionary Comprehension, create a results dictionary that takes each word in the given sentence,
+# and calculates the number of letters in each word.
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+
+results = {word: len(word) for word in sentence.split(' ')}
+print(results)
+
+# Use Dictionary Comprehension to create a dictionary called weather_f that takes each temperature
+# in degrees Celsius and converts it into degrees Fahrenheit.
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+weather_f = {day: (temp_c * 9 / 5) + 32 for day, temp_c in weather_c.items()}
+
+print(weather_f)
