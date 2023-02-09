048ba51cfdfa2e28c64bf49e51ab0252da74cdc7
Michael Nguyen <n93michael@gmail.com>
2023-02-09 11:03:27 -0500

    day 27 *args and *kwargs

--- 27-mile-kilo/lessons/main.py
+++ 27-mile-kilo/lessons/main.py
@@ -7,7 +7,7 @@ window.minsize(width=500, height=300)
 
 # Label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()  # The packer "packs" the code
+my_label.pack(side="left")  # The packer "packs" the code
 
 # This runs an infinite loop to keep our window running
 window.mainloop()
---
+++ 27-mile-kilo/lessons/playground.py
@@ -0,0 +1,40 @@
+# Unlimited Arguments
+
+# The * tells python to accept n amount of arguments
+
+# How can we modify the following add() function to take in unlimited arguments
+def add(*args):
+    total = 0
+    for n in args:
+        total += n
+
+    return total
+
+
+answer = add(1, 2, 3, 4, 5)
+
+
+# This creates unlimited keyword arguments
+
+# What will the following code output?
+def calculate(n, **kwargs):
+    # print(type(kwargs))  # kwargs
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+    # print(n)  # 25
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        # Notice how we use the .get method. This will leave the attribute optional
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+
+my_car = Car(make="Tesla", model="3")
+print(my_car.make)
