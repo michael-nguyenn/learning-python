ffa291e239ba15b85e0f28b4ba106c4e7a59e43f
Michael Nguyen <n93michael@gmail.com>
2023-02-11 15:33:19 -0500

    day 28 pomodoro timer - initial timer mechanism

--- 28-pomodoro/project/main.py
+++ 28-pomodoro/project/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,11 +11,29 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    # Calling the countdown
+    count_down(5 * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def reset_timer():
+    canvas.itemconfig(timer_text, text="00:00")
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    # Formatting the time
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -29,23 +48,14 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 # Have to use PhotoImage to use image
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 # Title Label
 label_title = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
 label_title.grid(column=1, row=0)
 
-
-# Button
-def start_timer():
-    print("hello!")
-
-
-def reset_timer():
-    print("oops")
-
-
+# Buttons
 start_btn = Button(text="START", command=start_timer, fg=GREEN, activeforeground=RED, highlightbackground=YELLOW,
                    padx=2, pady=2, font=(FONT_NAME, 16))
 start_btn.grid(column=0, row=2)
