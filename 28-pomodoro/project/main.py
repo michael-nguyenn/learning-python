74061ea5a4bd4207113f60936fcc4ad72dfc2e6f
Michael Nguyen <n93michael@gmail.com>
2023-02-11 15:15:14 -0500

    day 28 pomodoro timer - UI components added

--- 28-pomodoro/project/main.py
+++ 28-pomodoro/project/main.py
@@ -30,7 +30,35 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+# Title Label
+label_title = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
+label_title.grid(column=1, row=0)
+
+
+# Button
+def start_timer():
+    print("hello!")
+
+
+def reset_timer():
+    print("oops")
+
+
+start_btn = Button(text="START", command=start_timer, fg=GREEN, activeforeground=RED, highlightbackground=YELLOW,
+                   padx=2, pady=2, font=(FONT_NAME, 16))
+start_btn.grid(column=0, row=2)
+
+reset_btn = Button(text="RESET", command=reset_timer, fg=RED, highlightbackground=YELLOW, activeforeground=GREEN,
+                   padx=2, pady=2, font=(FONT_NAME, 16))
+reset_btn.grid(column=2, row=2)
+
+# Checkmark Label
+check_marks = "✔"
+
+check_label = Label(text=check_marks, bg=YELLOW, fg=GREEN)
+check_label.grid(column=1, row=3)
 
 # Allows window to run
 window.mainloop()
