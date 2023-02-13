62791582650087ffa573df91fa76432f2a5daada
Michael Nguyen <n93michael@gmail.com>
2023-02-12 21:31:56 -0500

    day 28 pomodoro timer - implemented break timers

--- 28-pomodoro/project/main.py
+++ 28-pomodoro/project/main.py
@@ -10,6 +10,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
@@ -17,7 +18,22 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     # Calling the countdown
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        label_title.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        label_title.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        label_title.config(text="Work", fg=GREEN)
 
 
 def reset_timer():
@@ -30,9 +46,18 @@ def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
+    # Dynamic Typing - Allows us to change typing
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    if count_sec == 0:
+        count_sec = "00"
+
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
