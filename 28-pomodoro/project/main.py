a9cd92b5cc0a03265b296230c5a6a6216feac116
Michael Nguyen <n93michael@gmail.com>
2023-02-13 19:05:26 -0500

    day 28 pomodoro project completed

--- 28-pomodoro/project/main.py
+++ 28-pomodoro/project/main.py
@@ -1,5 +1,5 @@
-from tkinter import *
 import math
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -11,13 +11,23 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
 def start_timer():
-    # Calling the countdown
     global reps
     reps += 1
 
@@ -27,60 +37,49 @@ def start_timer():
 
     if reps % 8 == 0:
         count_down(long_break_sec)
-        label_title.config(text="Break", fg=RED)
+        title_label.config(text="Break", fg=RED)
     elif reps % 2 == 0:
         count_down(short_break_sec)
-        label_title.config(text="Break", fg=PINK)
+        title_label.config(text="Break", fg=PINK)
     else:
         count_down(work_sec)
-        label_title.config(text="Work", fg=GREEN)
-
-
-def reset_timer():
-    canvas.itemconfig(timer_text, text="00:00")
+        title_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    # Formatting the time
     count_min = math.floor(count / 60)
     count_sec = count % 60
-
-    # Dynamic Typing - Allows us to change typing
     if count_sec < 10:
         count_sec = f"0{count_sec}"
 
-    if count_sec == 0:
-        count_sec = "00"
-
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-# Creating the window
 window = Tk()
-window.title("Pomodoro~")
+window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-# Canvas Widget - Allows images to be placed on top of each other
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
 
-# Have to use PhotoImage to use image
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-# Title Label
-label_title = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
-label_title.grid(column=1, row=0)
-
-# Buttons
 start_btn = Button(text="START", command=start_timer, fg=GREEN, activeforeground=RED, highlightbackground=YELLOW,
                    padx=2, pady=2, font=(FONT_NAME, 16))
 start_btn.grid(column=0, row=2)
@@ -89,11 +88,7 @@ reset_btn = Button(text="RESET", command=reset_timer, fg=RED, highlightbackgroun
                    padx=2, pady=2, font=(FONT_NAME, 16))
 reset_btn.grid(column=2, row=2)
 
-# Checkmark Label
-check_marks = "✔"
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
-check_label = Label(text=check_marks, bg=YELLOW, fg=GREEN)
-check_label.grid(column=1, row=3)
-
-# Allows window to run
 window.mainloop()
